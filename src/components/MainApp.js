afd862983f64d8560d8105501bcb54d3fe316668
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2017-06-19 16:28:39 -0500

    Fix to show semi-correct on mobile devices

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -16,8 +16,8 @@ class MainApp extends React.Component {
 
       <div className={styles.main}>
         <Grid container>
-          <Grid item sm={3} />
-          <Grid item sm={6}>
+          <Grid item xs={0} sm={3} />
+          <Grid item xs={12} sm={6}>
             <SearchResults />
           </Grid>
         </Grid>
